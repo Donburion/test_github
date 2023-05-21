252bc37a9f933e3b0ddfda7ff268a1322bfb4b99
kobayashi <otyanoazi@gmail.com>
2023-05-21 15:13:30 +0900

    add more creatives.

--- gui.py
+++ gui.py
@@ -1,5 +1,9 @@
 import tkinter as tk
+def terminate_this(root):
+    root.destroy()
 
 if __name__ == '__main__':
     root = tk.Tk()
+    new_button = tk.Button(master = root, text = 'new_button', command = lambda : terminate_this(root))
+    new_button.pack()
     root.mainloop()
